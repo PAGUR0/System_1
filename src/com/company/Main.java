6a5e98715baa1e8eb9e669f7a883cb184b594af8
Paguro <ivankosaev2006@gmail.com>
2023-09-02 17:46:17 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] numbers = new int[1000];
+        Random random = new Random();
+
+	    for(int i = 0;i < numbers.length;i++){
+	        numbers[i] = random.nextInt(10000);
+        }
+
+	    int max = 0;
+	    int max_2 = 0;
+	    int max_7 = 0;
+	    int max_14 = 0;
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
+            }
+            else if ((number % 2 == 0) && (number > max_2)) {
+                max_2 = number;
+            }
+            else if ((number % 7 == 0) && (number > max_7)) {
+                max_7 = number;
+            }
+            else if ((number % 14 == 0) && (number > max_14)) {
+                max_14 = number;
+            }
+        }
+	    int product_max = max * max_14;
+	    int product_max_2 = max_2 * max_7;
+	    if (product_max * product_max_2 == 0){
+            System.out.println(-1);
+        }
+	    else{
+            System.out.println(Math.max(product_max, product_max_2));
+        }
+    }
+}
